992a5a190281c539714138b1c0f827c47d7fd4a8
Dubskey <DUBBYW.PALADINO@GMAIL.COM>
2024-09-15 12:00:17 -0700

    Unity Project Created, SpellCard Scriptable object created

---
+++ Comet/Assets/Scripts/SpellCard.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New SpellCard", menuName = "SpellCard")]
+public class SpellCard : ScriptableObject
+{
+    public enum rarity
+    {
+        common,
+        rare,
+        epic,
+        legendary
+    }
+
+    public enum type
+    {
+        basic,
+        special
+    }
+
+    public string spellName;
+    public string description;
+
+    public int numberOfUses;
+    public int power;
+    public int castDelay;
+    public int coolDown;
+    public rarity cardRarity;
+    public type cardType;
+}
