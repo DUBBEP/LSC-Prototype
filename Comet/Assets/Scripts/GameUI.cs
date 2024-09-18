1e8158b9e85e9d3880c258925fa278f4503c9706
Dubbyw <106718081+DUBBEP@users.noreply.github.com>
2024-09-18 08:12:48 -0700

    Small Changes

--- Comet/Assets/Scripts/GameUI.cs
+++ Comet/Assets/Scripts/GameUI.cs
@@ -1,9 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class GameUI : MonoBehaviour
 {
+    GameObject spellHandUI;
+
+
+    // instance
+    public static GameUI instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +25,10 @@ public class GameUI : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void ToggleHandUI()
+    {
+        spellHandUI.SetActive(!spellHandUI.activeSelf);
     }
 }
