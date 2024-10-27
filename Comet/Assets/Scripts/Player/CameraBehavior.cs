3e1af7f5750c9baf32ede7d1de2e9d31d2c1f355
Dubskey <DUBBYW.PALADINO@GMAIL.COM>
2024-10-26 20:41:00 -0700

    Revert "New Camera Features"
    This reverts commit 95e597c0a596f542e03be7ddbe0fd1500b871e2a, reversing
    changes made to 9f45e6b73d0a1992dd4c5921b6f353b5b57a73eb.

--- Comet/Assets/Scripts/Player/CameraBehavior.cs
+++ Comet/Assets/Scripts/Player/CameraBehavior.cs
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 using Photon.Realtime;
-=======
->>>>>>> 06666ae0623eb485cd3bb362254f535f4aa2747e
 using UnityEngine;
 
 public class CameraBehavior : MonoBehaviour
@@ -11,7 +8,6 @@ public class CameraBehavior : MonoBehaviour
     public float smoothFactor = 4;
 
     private bool isFollowing;
-<<<<<<< HEAD
     private bool returnToRest;
     private Transform target;
     public Transform Target { get { return target; } }
@@ -121,58 +117,10 @@ public class CameraBehavior : MonoBehaviour
         isFollowing = true;
         target = targetTransform;
         targetPosition = new Vector3(target.position.x, camOffSet.y, target.position.z - 1);
-=======
-    private bool returnToDefaultPos;
-    private Transform target;
-    public Transform Target { get { return target; } }
-
-    private Vector3 targetPosition;
-    private float verticalOffSet;
-    private float defaultFOV;
-    private float targetFOV;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        _camera = GetComponent<Camera>();
-        verticalOffSet = GameManager.instance.camDefaultPos.y;
-        defaultFOV = _camera.fieldOfView;
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (isFollowing)
-        {
-            transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smoothFactor);
-
-            _camera.fieldOfView = Mathf.Lerp(_camera.fieldOfView, targetFOV, Time.deltaTime * smoothFactor);
-
-        }
-
-        if (returnToDefaultPos)
-        {
-            transform.position = Vector3.Lerp(transform.position, GameManager.instance.camDefaultPos, Time.deltaTime * smoothFactor);
-
-            _camera.fieldOfView = Mathf.Lerp(_camera.fieldOfView, targetFOV, Time.deltaTime * smoothFactor);
-
-        }
-    }
-
-
-    public void StartFollowing(Transform targetTransform)
-    {
-        Debug.Log("Start Following");
-        isFollowing = true;
-        returnToDefaultPos = false;
-        target = targetTransform;
-        targetPosition = new Vector3(target.position.x, verticalOffSet, target.position.z - 1);
->>>>>>> 06666ae0623eb485cd3bb362254f535f4aa2747e
         targetFOV = 45;
 
     }
 
-<<<<<<< HEAD
     public void RestAtPlayerQuadrant()
     {
         isFollowing = false;
@@ -211,14 +159,4 @@ public class CameraBehavior : MonoBehaviour
     }
 
     #endregion
-=======
-    public void StopFollowing()
-    {
-        isFollowing = false;
-        returnToDefaultPos = true;
-        target = null;
-        targetPosition = Vector3.zero;
-        targetFOV = defaultFOV;
-    }
->>>>>>> 06666ae0623eb485cd3bb362254f535f4aa2747e
 }
